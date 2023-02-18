e2e46a98be0689179b09afaeedbc0e25c5c100a1
grifb <114772878+grifb@users.noreply.github.com>
2023-02-18 12:00:33 -0500

    added max hearts

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,9 +4,12 @@ using UnityEngine.UIElements;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("The maximum number of full hearts the player can have.")]
+    private int _maxHearts;
     private int _maxHealth;
     [SerializeField]
-    [Range(0, 10)]
+    [Tooltip("One unit of health is half a heart.")]
+    [Range(0, 20)]
     private int _health;
     private UIDocument _doc;
 
@@ -20,33 +23,39 @@ public class HealthBar : MonoBehaviour
         CalculateHearts();
     }
 
-    void Update() {
-        CalculateHearts(); 
-    } 
+    void Update()
+    {
+        CalculateHearts();
+    }
 
-    void CalculateHearts() {
+    void CalculateHearts()
+    {
         var root = _doc.rootVisualElement;
         var heartContainer = root.Q<VisualElement>("heart-container");
         heartContainer.Clear();
 
+        _maxHealth = _maxHearts * 2;
         var halfHearts = _health % 2;
         var fullHearts = (_health - halfHearts) / 2;
         var emptyHearts = (_maxHealth / 2) - (halfHearts + fullHearts);
 
-        for (int i = 0; i < fullHearts; i++) {
-           AddHeart(heartContainer, fullHeartClassName); 
-        } 
-
-        if (halfHearts > 0) {
-           AddHeart(heartContainer, halfHeartClassName); 
+        for (int i = 0; i < fullHearts; i++)
+        {
+            AddHeart(heartContainer, fullHeartClassName);
         }
 
-        for (int i = 0; i < emptyHearts; i++) {
+        if (halfHearts > 0)
+        {
+            AddHeart(heartContainer, halfHeartClassName);
+        }
+
+        for (int i = 0; i < emptyHearts; i++)
+        {
             AddHeart(heartContainer, emptyHeartClassName);
         }
- 
+
     }
-     
+
     void AddHeart(VisualElement heartContainer, string type)
     {
         var heart = new VisualElement();
