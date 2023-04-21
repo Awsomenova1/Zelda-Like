2bcf492a67de3e07ae18197125980783965f6d2a
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-04-21 16:53:36 -0400

    Modified NPC to find player script
    NPC now uses Find function to find NPC Dialogue component of player on start

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -6,11 +6,10 @@ public class NPC : MonoBehaviour
     [SerializeField] private NPCDialogue _npcDialogue;
     [SerializeField] private string npcText;
 
-    // private void Start()
-    // {
-    //     var dialogue = GameObject.Find("Canvas");
-    //     _npcDialogue = dialogue.transform.GetChild(1).GetComponent<NPCDialogue>();
-    // }
+    private void Start()
+    {
+        _npcDialogue = GameObject.Find("Player").GetComponent<NPCDialogue>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
