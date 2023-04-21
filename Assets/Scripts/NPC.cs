5bf2df4c24f8f282430ced4732ee5072c44a6b2d
grifb <114772878+grifb@users.noreply.github.com>
2023-04-21 13:39:33 -0400

    added NPC dialogue

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private NPCDialogue _npcDialogue;
+    [SerializeField] private string _npcText;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _npcDialogue.SetDialogueText(_npcText);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+       _npcDialogue.RemoveDialogue(); 
+    }
+}
---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NPCDialogue : MonoBehaviour
+{
+    [SerializeField] private TMP_Text dialogueText;
+    private bool _npcNearby;
+    private bool _isShowingDialogue;
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (_npcNearby && (!_isShowingDialogue)) 
+        {
+            DisplayDialogue();
+        }
+        else
+        {
+            _isShowingDialogue = false;
+            gameObject.SetActive(false);
+        }
+    }
+    
+    public void SetDialogueText(string text)
+    {
+        dialogueText.text = text;
+        _npcNearby = true;
+    }
+
+    public void RemoveDialogue()
+    {
+        _npcNearby = false;
+    }
+
+    private void DisplayDialogue()
+    {
+        _isShowingDialogue = true;
+        gameObject.SetActive(true); 
+    }
+}
