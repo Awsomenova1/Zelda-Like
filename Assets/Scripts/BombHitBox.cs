6b881c3af32c21e3835c41cb61f9a70c5516cf1a
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-10-06 16:40:11 -0400

    Added BombHitbox to player and scripts

--- Assets/Scripts/BombHitBox.cs
+++ Assets/Scripts/BombHitBox.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHitBox : MonoBehaviour
+public class BombHitBox : MonoBehaviour
 {
     [SerializeField]
     int _damage = 4;
+    int _damageToPlayer = 2;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Enemy")){
@@ -14,6 +15,11 @@ public class PlayerHitBox : MonoBehaviour
         else if(other.CompareTag("Breakable")){
             other.GetComponent<Breakable>().takeDamage(_damage);
         }
+        else if(other.CompareTag("Player")){
+            other.GetComponent<PlayerStatistics>().takeDamage(_damageToPlayer);
+        }
+        else if(other.CompareTag("Bombable")){
+        //    other.GetComponent<Bombable>().takeDamage(_damage);
+        }
     }
-    
 }
