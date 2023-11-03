84253b9cda6b89a8e8455d463057e3da1e190e33
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-11-03 16:15:05 -0400

    Added camera clamping

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,6 +8,9 @@ public class CameraMovement : MonoBehaviour
     public Transform target;
     public float smoothing;
 
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +22,15 @@ public class CameraMovement : MonoBehaviour
     //LateUpdate is used here instead of Update so all changes that the camera might see will occur before the camera updates to view it
     void LateUpdate()
     {
+        //move the camera to follow the target (usually the player)
         if(transform.position != target.position){
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //limit the camera to particular bounds
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+            
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
         }
     }
 }
