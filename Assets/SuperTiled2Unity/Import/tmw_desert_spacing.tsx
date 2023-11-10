fb295946da5e08735e62108392f82e75cd5cdef2
Awsomenova1 <91033039+Awsomenova1@users.noreply.github.com>
2023-11-10 16:55:38 -0500

    Added new dungeon scene

--- Assets/SuperTiled2Unity/Import/tmw_desert_spacing.tsx
+++ Assets/SuperTiled2Unity/Import/tmw_desert_spacing.tsx
@@ -3,7 +3,7 @@
  <image source="tmw_desert_spacing.png" width="265" height="199"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="16.125" y="16" width="15.875" height="16"/>
+   <object id="1" x="16" y="16" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1">
@@ -13,7 +13,7 @@
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="15.875" width="15.875" height="16.125"/>
+   <object id="1" x="0" y="16.0035" width="16" height="15.9965"/>
   </objectgroup>
  </tile>
  <tile id="8">
@@ -23,51 +23,51 @@
  </tile>
  <tile id="10">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="15.9864" height="32"/>
+   <object id="1" x="0" y="0" width="16" height="32"/>
   </objectgroup>
  </tile>
  <tile id="16">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="15.75" y="0" width="16.25" height="17.125"/>
+   <object id="1" x="16" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="17">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="17.0049"/>
+   <object id="1" x="0" y="0" width="32" height="16"/>
   </objectgroup>
  </tile>
  <tile id="18">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="17.125"/>
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="19">
   <objectgroup draworder="index" id="2">
-   <object id="5" x="0" y="0" width="32" height="16.9844"/>
-   <object id="4" x="0" y="0" width="15.9922" height="32"/>
+   <object id="5" x="0" y="0" width="32" height="16"/>
+   <object id="4" x="0" y="0" width="16" height="32"/>
   </objectgroup>
  </tile>
  <tile id="20">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="17.0039"/>
+   <object id="1" x="0" y="0" width="32" height="16"/>
    <object id="2" x="16" y="0" width="16" height="32"/>
   </objectgroup>
  </tile>
  <tile id="27">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="32"/>
-   <object id="2" x="0" y="16.0054" width="32" height="15.9946"/>
+   <object id="2" x="0" y="16" width="32" height="16"/>
   </objectgroup>
  </tile>
  <tile id="28">
   <objectgroup draworder="index" id="2">
    <object id="1" x="16" y="0" width="16" height="32"/>
-   <object id="2" x="0" y="16.0112" width="32" height="15.9888"/>
+   <object id="2" x="0" y="16" width="32" height="16"/>
   </objectgroup>
  </tile>
  <tile id="30">
   <objectgroup draworder="index" id="3">
-   <object id="4" x="4.76342" y="5.44139" width="22.201" height="19.3965">
+   <object id="4" x="4.75" y="5.45" width="22.201" height="19.3965">
     <polygon points="0.169473,-0.508204 22.201,-0.592905 22.201,19.3965 0,19.3965"/>
    </object>
   </objectgroup>
